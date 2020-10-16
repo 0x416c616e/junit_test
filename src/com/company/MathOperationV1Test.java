b945153ec76c487cf481d6d23657807b0f6120a5
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-10-16 01:23:59 -0500

    wrote unit test for MathTest class

--- src/com/company/MathOperationV1Test.java
+++ src/com/company/MathOperationV1Test.java
@@ -1,24 +1,37 @@
 package com.company;
 
+
+
+
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class MathOperationV1Test {
 
+    MathTest mathTester = new MathTest();
+
     @org.junit.jupiter.api.Test
     void add() {
-         MathTest test = new MathTest();
-
+        int result = mathTester.add(2, 2);
+        assertEquals(4, result);
     }
 
     @org.junit.jupiter.api.Test
     void subtract() {
+        double result = mathTester.subtract(1.25, 0.5);
+        assertEquals(0.75, result);
     }
 
     @org.junit.jupiter.api.Test
     void multiply() {
+        int result = mathTester.multiply(2,3);
+        assertEquals(6, result);
     }
 
     @org.junit.jupiter.api.Test
     void divide() {
+        int result = mathTester.divide(10,2);
+        assertEquals(5, result);
     }
 }
